749d49e78baa098e43c580847234ee77747b0995
kvsco <78269661+kvsco@users.noreply.github.com>
2021-09-13 23:17:33 +0900

    Update 01_dfs.py

--- 이코테/03_DFS_BFS/01_dfs.py
+++ 이코테/03_DFS_BFS/01_dfs.py
@@ -1,8 +1,29 @@
 # -*- coding: utf-8 -*-
-def factorial(n):
-    if n <= 1:
-        return 1
-    
-    return n * factorial(n-1)
+def dfs(graph, v, visited):
+    #현재노드 방문처리
+    visited[v] = True
+    print(v)
 
-print(factorial(6))
+    #현재노드와 연결된 다른노드 재귀호출
+    for i in graph[v]:
+        print('현재i')
+        print(i)
+        if not visited[i]: # False이면 방문 X, True 이면 방문한 노드임.
+            dfs(graph, i, visited)
+
+#인접리스트 graph
+graph = [
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
+visited = [False] * 9
+#print(visited)
+#노드 1번부터 시작
+dfs(graph, 1, visited)
