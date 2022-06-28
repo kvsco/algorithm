2cda454d417c36cace40b54d585e4d2a495dbaa4
kvsco <nngjk@naver.com>
2022-06-28 22:35:01 +0900

    코딩테스트 복기

--- 코테복기/Nfactorial.py
+++ 코테복기/Nfactorial.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*- 
+import sys
+sys.setrecursionlimit(10**6)
+
 def factorial(result):
     print(result)
 
@@ -6,7 +10,7 @@ def factorial(result):
     else:
         return 1
 
-n = factorial(996)
+n = factorial(100000)
 print(n)
 
 # 최대 깊이 n = 995 ,  996부터 RecursionError: maximum recursion depth exceeded while calling a Python object
